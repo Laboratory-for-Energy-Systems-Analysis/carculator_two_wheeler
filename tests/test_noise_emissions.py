49b20c7754c6a5b3ae00c74533aeb3a2a7cacd24
romainsacchi <r_s@me.com>
2021-09-01 12:15:13 +0200

    Initial commit

---
+++ tests/test_noise_emissions.py
@@ -0,0 +1,23 @@
+import pytest
+
+from carculator_two_wheeler.driving_cycles import get_standard_driving_cycle
+from carculator_two_wheeler.noise_emissions import NoiseEmissionsModel
+
+dc = get_standard_driving_cycle()
+dc_name = "WLTC"
+
+
+def test_wrong_powertrain():
+    nem = NoiseEmissionsModel(dc, dc_name)
+    with pytest.raises(TypeError) as wrapped_error:
+        nem.get_sound_power_per_compartment("foo")
+    assert wrapped_error.type == TypeError
+
+
+def test_output_emissions():
+    nem = NoiseEmissionsModel(dc, dc_name)
+    urban = nem.get_sound_power_per_compartment("combustion")[0]
+
+    assert len(urban) == 8
+    assert urban.sum() > 0.09
+    assert urban.sum() < 0.1
